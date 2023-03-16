2ecf9626c86e73c3b5acdc00b2054eaa12f39878
marcosalvaradomartinez <mam02@iesemilidarder.com>
2023-03-16 20:26:37 +0100

    Añado los cambios de tabla

--- tabla.py
+++ tabla.py
@@ -1,7 +1,17 @@
 #crearemos una tabla de multiplicar
-numero=int(input("Pon un numero: "))
+# numero=int(input("Pon un numero: "))
+#i=1
+# while i <=10:
+#     # print(+numero*i)
+#     print(i,"x",numero,"=",numero*i)
+#     i=i+1
+
 i=1
-while i <=10:
-    # print(+numero*i)
-    
-    i=i+1
+j=1
+while j<=10:
+    while i<=10:
+        print(i, "x", j, "=", j*i)
+        i=i+1
+    j=j+1
+    print("--------------")
+    i=1
